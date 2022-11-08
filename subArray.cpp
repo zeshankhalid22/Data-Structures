69f0f36c3696582c04062bd2369bb230abdc0bb5
Zeshan Khalid <imzeshankhalid@gmail.com>
2022-11-08 19:13:54 +0500

    sorting subArrays sum

--- subArray.cpp
+++ subArray.cpp
@@ -1,34 +1,34 @@
+// * replacing SubArray's smallest to largest Sum by itself using Cumulative approach
+// arr={1,2,3}  → arr={1,3,6} by 1,2+1,3+2+1
 #include "iostream"
 using namespace std;
-// * How to print sub Array elements
 // * Sub Array elements are contiguous elements of an array
-void sum_sub_array(int n){ // Largest sum Among subArrays using brute-force approach
+void sum_sub_array(int n){ // Largest sum Among subArrays using Cumulative approach
     int arr[n];
     cout<<"Array input till "<<n<<": ";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    int sum;
-    int sum2=0;
+// Traversing Array
+    cout<<"Before sum: { ";
     for(int i=0;i<n;i++){
-        sum=0;
-        for(int j=i;j<n;j++){
-            for(int k=i;k<=j;k++){
-                sum +=arr[k]; // Add sum of a subArray
-//                cout<<arr[k]<<" "; // print SubArrays
-            }
-            if(sum>sum2){           // if sum is getting bigger then sum2(previous sum)
-                sum2=sum;           // it's value is stored in sum2.
-                sum=0;              // sum will be 0 to store next subArray's values
-                                    // for availability to compare it with previous great sum in (sum2)
-            }
-//            cout<<endl;
-        }
+        cout<<arr[i]<<", ";
     }
-    cout<<"Largest sum among subArray using brut-force approach: "<<sum2<<endl;
+
+    for(int i=1;i<n;i++) {
+        arr[i] += arr[i-1];  // Any index += with their previous index
+    }
+// Traversing Array
+  cout<<"}\n After sum: { ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<", ";
+    }
+// As we are adding element to the next element, sum will be the last element
+cout<<"}\nLargest Sum is Last element: "<<arr[n-1]<<endl;
+    // ! Note! it will only sort and calculate largest sum for Positive numbers only
 }
 int main(){
     int n;
     cin>>n;
-    sum_sub_array(n);// Print Sum and subArray for a given length
+    sum_sub_array(n);// Print Sums of subArrays for a given length
 }
