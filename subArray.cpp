d3f55cb03d4884d4e40a45164c37e4db0cd58e73
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2022-11-20 10:36:42 +0500

    kadane algorithm added
    complexity menu added to ask in which complexity you wanna solve

--- subArray.cpp
+++ subArray.cpp
@@ -2,7 +2,7 @@
 #include "iostream"
 using namespace std;
 
-void max_sum_of_subArray(int n){ // Largest sum Among subArrays using Brut force approach
+void max_sum_BrutForce(int n){ // O(N^3) Largest sum Among subArrays using Brut force approach.
     int arr[n];
     cout<<"\nFinding Largest sum Among subArrays using Brut force approach O(n^3)"<<endl;
     cout<<"input array elements for max_sum_of_subArray(): ";
@@ -16,7 +16,7 @@ void max_sum_of_subArray(int n){ // Largest sum Among subArrays using Brut force
             sum=0;
             cout<<"subArray {";   // to merge with next arr[k]
             for(int k=i;k<=j;k++){
-               cout<<" "<<arr[k]; // print subArray elements in a row
+                cout<<" "<<arr[k]; // print subArray elements in a row
                 sum+=arr[k];                // sum of that subArray
             }
             cout<<" } (sum: ) "<<sum;
@@ -35,11 +35,11 @@ void max_sum_of_subArray(int n){ // Largest sum Among subArrays using Brut force
     cout<<"\nFinally "<<greaterSum<<" is the Largest Sum of SubArray!\n";
 }
 
-void sum_sub_array(int n){ // Largest sum Among subArrays using Cumulative approach
+void max_sum_Commulative(int n){ // O(N^2) Largest sum Among subArrays using Cumulative approach
 // * replacing SubArray's smallest to the largest Sum by itself using Cumulative approach
 // arr={1,2,3}  → arr={1,3,6} by 1,2+1,3+2+1
     int arr[n];
-     cout<<"\nFinding Largest sum Among subArrays using Cumulative approach O(n)\n";
+    cout<<"\nFinding Largest sum Among subArrays using Cumulative approach O(n)\n";
     cout<<"Array elements for sum_sub_array "<<n<<": ";
     for(int i=0;i<n;i++){
         cin>>arr[i];
@@ -54,19 +54,42 @@ void sum_sub_array(int n){ // Largest sum Among subArrays using Cumulative appro
         arr[i] += arr[i-1];  // Any index += with their previous index
     }
 // Traversing Array
-  cout<<"}\n After sum: { ";
+    cout<<"}\n After sum: { ";
     for(int i=0;i<n;i++){
         cout<<arr[i]<<", ";
     }
 // As we are adding element to the next element, sum will be the last element
-cout<<"}\nFinally Last element "<<arr[n-1]<<" is the Largest Sum of SubArray!\n";
+    cout<<"}\nFinally Last element "<<arr[n-1]<<" is the Largest Sum of SubArray!\n";
     // ! Note! it will only sort and calculate the largest sum for Positive numbers only
 }
-
+int max_sum_kadane(int n){ // O(N) maximum sum using kadane algorithm
+    int Sum=0,currSum=0;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        currSum+=arr[i];
+        if(currSum<0)
+            currSum=0;
+        if(currSum>Sum)
+            Sum=currSum;
+    }
+    return Sum;
+}
 int main(){
     int n;
-    cout<<"Enter the desired number: ";
+    char complexity;
+    cout<<"Enter the number of elements: ";
     cin>>n;
-    sum_sub_array(n);// Print Sums of subArrays for a given length
-    max_sum_of_subArray(n); // Display maximum sum of SubArray
+    cout<<"Which complexity you wanna use \n1. O(N^3)\n2.O(N^2)\n3.O(N)\n";
+    cin>>complexity;
+   if(complexity=='1')
+       max_sum_BrutForce(n);
+   else if(complexity=='2')
+       max_sum_Commulative(n);
+   else if(complexity=='3')
+       cout<<max_sum_kadane(n);
+   else
+       cout<<"Wrong input.";
 }
