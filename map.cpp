503835f1fe90e5157a54ae653cc01983a8f8a6d3
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2023-03-30 21:49:22 +0500

    unordered map added

--- map.cpp
+++ map.cpp
@@ -1,17 +1,24 @@
 #include "iostream"
 #include "map"
+#include "unordered_map"
 using namespace std;
 
-    // Function to print size and key:value pair
-    void print(map<string,int>&Map){
-        cout<<"Map size: "<<Map.size()<<endl;   // Size of Map
-        for(auto &it:Map){  // Printing Values
-            cout<<"Key: "<<it.first<<", Value: "<<it.second<<endl;
-        }
+// Function to printMap size and key:value pair
+void printMap(map<string,int>&Map){
+    cout<<"Map size: "<<Map.size()<<endl;   // Size of Map
+    for(auto &it:Map){  // Printing Values
+        cout<<"Key: "<<it.first<<", Value: "<<it.second<<endl;
     }
-int main(){
-    map<string,int> Map;
+}
+void printUmap(unordered_map<string,int>&umap){
+    cout<<"\n\t- - - - - -\n";
+    for(auto &it:umap){
+        cout<<"Key: "<<it.first<<", Value: "<<it.second<<endl;
+    }
+}
 
+void ordered_map(){
+    map<string,int> Map;
     //  Map to Store Stationary keys along with their corresponding frequency
     Map["pointer"]=3;
     Map["ballpen"]=2;
@@ -20,7 +27,7 @@ int main(){
     Map["register"]=6;  // Value will be replaced
     Map["mouse"];   // "mouse" is inserted in Map  with an empty string value
 
-    print(Map); // Calling print function
+    printMap(Map); // Calling printMap function
     auto it2 = Map.find("mouse"); // Find value of inserted key
     cout<<"\nFind Value.  Key: "<<it2->first<<", Value: "<<it2->second<<endl;
 
@@ -31,7 +38,36 @@ int main(){
     Map.erase(it);       // Delete the whole pair of "marker"
 
     cout<<"\n - - - Printing Again - - -\n";
-    print(Map);
+    printMap(Map);
 
+}
 
+void unorderedMap(){
+    // Declare unordered map and initilize 2 pairs
+    unordered_map<string,int> umap{
+            {"DLD",60},
+            {"Islamyat",75},
+    };
+
+    cout<<"Old Marks: "<<endl;
+    cout<<"DLD : "<<umap["DLD"]<<endl
+        <<"Islamyat: "<<umap["Islamyat"]<<endl;
+
+        // * Updating Values
+        umap["DLD"]=66; // [] operator assign Garbage value is specified key doesn't exist
+        umap.at("Islamyat") += 10;  // increment value by 10
+
+        // * Inserting Pairs
+        umap["Math"]=55;
+        umap.insert({{"English",77},{"OOP",87}});
+
+        // * Displaying Elements
+    printUmap(umap);
+
+}
+int main(){
+    // Functions demonstrating ordered and unordered map
+    
+    ordered_map();
+//    unorderedMap();
 }
