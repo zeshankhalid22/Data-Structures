14a8ab1411bb86998f040d09a25cbe8eea2b0183
zeshankhalid22 <116242001+zeshankhalid22@users.noreply.github.com>
2023-12-15 10:31:58 +0500

    Updated

--- Graph_data_strucuture/Detect_Cycle_Directed_Graph.cpp
+++ Graph_data_strucuture/Detect_Cycle_Directed_Graph.cpp
@@ -3,139 +3,91 @@
 #include <vector>
 #include "queue"
 
+
 using namespace std;
 
-class DirectedGraph {
+class Graph {
+    bool isDirected;
     unordered_map<int, vector<int>> adjList;
 
-    // Check whether some Path is tried to visited again (DFS)
-    bool path_vis_again(int v, unordered_map<int, bool> &visited, unordered_map<int, bool> &path) {
-
-        // visit and set current V's path
-        visited[v] = true;
-        path[v] = true;
-        for (auto neighbours: adjList[v]) {
-            if (!visited[neighbours]) {
-                // go in neighbours path
-                bool ret = path_vis_again(neighbours, visited, path);
-                if (ret)
-                    return true;
-            }
-                // if we are coming [again] to visited, path. it is Cycle
-            else if (path[neighbours]) {
-                return true;
-            }
-        }
-        // leaving vertex, mean leaving this path
-        path[v] = false;
-        return false;
-    }
-
 public:
+    Graph(bool isDirected) : isDirected(isDirected) {}
 
     void addEdge(int u, int v) {
         adjList[u].push_back(v);
+        if (!isDirected)
+            adjList[v].push_back(u); // Comment this line for a directed graph
     }
 
-    // topological sort using bfs, also check whether cycle detected or not
-    bool kahns_algo() {
+    // topological sort using bfs
+    void kahns_algo() {
+        if (!isDirected) {
+            cout << "No Topological Sort (UnDirected graph).\n";
+        }
         //        [vertex, degree]
         unordered_map<int, int> inDegree;
-        int count = 0;
-        
         // initial degree of each node = 0
         for (const auto &i: adjList)
             inDegree[i.first] = 0;
 
         // traverse and add inDegree to each Vertex
-        for (auto &i: adjList) {
-            for (auto &j: i.second) {
-                // Increment inDegree of each adjacent vertex
+        for (const auto &i: adjList) {
+            for (int j: i.second) {
                 inDegree[j]++;
             }
         }
-
+        vector<int> topSort;   // to store elements in topological order
         queue<int> zeroInDegreeQueue;
-
         // Pushing all 0 inDegree elements in queue
-        //         [ vertex, degree ]
-        for (auto i: inDegree)
-            if (i.second == 0)
+        for (auto &i: inDegree) {
+            if (i.second == 0) {
+                inDegree[i.first]--;
                 zeroInDegreeQueue.push(i.first);
+            }
+        }
+
 
         while (!zeroInDegreeQueue.empty()) {
             int front = zeroInDegreeQueue.front();
             zeroInDegreeQueue.pop();
 
-            count++;
-            // insert neighbours of front
+            topSort.push_back(front);
+            // insert neighbours
             for (auto neighbours: adjList[front]) {
-                inDegree[neighbours]--; // reduce neighbours inDegree
-                if (inDegree[neighbours] == 0)
+                inDegree[neighbours]--;
+                if (inDegree[neighbours] < 0) {
+                    cout << "Cycle detected.\n";
+                    return;
+                }
+                if (inDegree[neighbours] == 0) {
                     zeroInDegreeQueue.push(neighbours);
+                }
             }
         }
 
-        // check whether all the elements are counted
-        // if not, it means Cycle exist
-        if (this->adjList.size() == count)
-            return true;
-        else
-            return false;
-    }
-
-    bool isCycle() {
-        int V = adjList.size();
-        unordered_map<int, bool> visited;
-        unordered_map<int, bool> path;
-
-        for (int i = 0; i < V; i++) {
-            // check for every non-visited node
-            if (!visited[i]) {
-                bool ret = path_vis_again(i, visited, path);
-                if (ret)
-                    return ret;
-            }
+        if (topSort.size() != adjList.size()) {
+            cout << "Cycle Detected.\n";
+            return;
         }
-        return false;
+// if loop completed, it means Cycle is not Detected
+        cout << "Topological Sort -> ";
+        for (auto &i: topSort) {
+            cout << i << " ";
+        }
+        cout <<endl;
     }
-
 
 };
 
 int main() {
-    DirectedGraph graph;
-    unsigned int edges;
-    cout << "Enter number of Edges > ";
-    cin >> edges;
-    cout << "Enter " << edges << " (u -> v) pairs:\n";
-    while (edges--) {
-        int u, v;
-        cout << " > ";
-        cin >> u >> v;
-        graph.addEdge(u, v);
-    }
+    bool isDirected = true;
+    Graph graph(isDirected);
+    graph.addEdge(4, 11);
+    graph.addEdge(11, 13);
+    graph.addEdge(13, 15);
+//    graph.addEdge(13,4);
+    graph.addEdge(7,8);
 
-    int choice;
-    cout << "Wanna detect Cycle through [0. DFS,  1. BFS] > ";
-    cin >> choice;
-    if (choice == 0) {
-        bool ans = graph.isCycle();
-        if (ans) {
-            cout << "Cycle Detected.\n";
-            return 0;
-        }
-        cout << "Cycle not Detected\n";
-    } else if (choice == 1) {
-        if (graph.kahns_algo()) {
-            cout << "Cycle Detected.\n";
-            return 0;
-        }
-        cout << "Cycle not Detected\n";
-    } else {
-        cout << "No other Option.\n";
-        return 0;
-    }
-
+    graph.kahns_algo();
 
 }
